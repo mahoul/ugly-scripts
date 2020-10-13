314273e73abfcaea4f0109eca3300b67be09ad18
Enrique V. Gil Palau <enrique.gil@ams-ix.net>
2020-10-13 17:46:58 +0200

    Fixes on setup-apache-dav.sh

--- setup-apache-dav.sh
+++ setup-apache-dav.sh
@@ -50,49 +50,12 @@ if is_debian || is_ubuntu; then
         a2enmod dav
         a2enmod dav_fs
 
-
         # Add basic WebDav config to default Apache site
         #
-        cat <<-EOF > /etc/apache2/sites-enabled/000-default.conf
-        DavLockDB /var/www/DavLock
-        <VirtualHost *:80>
-                # The ServerName directive sets the request scheme, hostname and port that
-                # the server uses to identify itself. This is used when creating
-                # redirection URLs. In the context of virtual hosts, the ServerName
-                # specifies what hostname must appear in the request's Host: header to
-                # match this virtual host. For the default virtual host (this file) this
-                # value is not decisive as it is used as a last resort host regardless.
-                # However, you must set it for any further virtual host explicitly.
-                #ServerName www.example.com
-
-                ServerAdmin webmaster@localhost
-                DocumentRoot /var/www/html
-
-                # Available loglevels: trace8, ..., trace1, debug, info, notice, warn,
-                # error, crit, alert, emerg.
-                # It is also possible to configure the loglevel for particular
-                # modules, e.g.
-                #LogLevel info ssl:warn
-
-                ErrorLog ${APACHE_LOG_DIR}/error.log
-                CustomLog ${APACHE_LOG_DIR}/access.log combined
-
-                # For most configuration files from conf-available/, which are
-                # enabled or disabled at a global level, it is possible to
-                # include a line for only one particular virtual host. For example the
-                # following line enables the CGI configuration for this host only
-                # after it has been globally disabled with "a2disconf".
-                #Include conf-available/serve-cgi-bin.conf
-
-            Alias /dav /var/www/dav
-            <Directory /var/www/dav>
-                    DAV On
-            </Directory>
-
-        </VirtualHost>
-
-        # vim: syntax=apache ts=4 sw=4 sts=4 sr noet
-        EOF
+        sed -i \
+        -e '1s#^#DavLockDB /var/www/dav\n#' 
+        -e 's#</VirtualHost>#\n\tAlias /dav /var/www/dav\n\t<Directory /var/www/dav>\n\t\tDAV On\n\t</Directory>\n</VirtualHost>#' \
+        000-default.conf
 
         # Restart Apache2 service
         systemctl restart apache2
